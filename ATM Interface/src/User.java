bf05e3429464e5f5a64ee3c11fedb1c060c3ad02
Sanjay Krishnan <161303051+imsanjaykrishnan@users.noreply.github.com>
2024-03-04 12:44:42 +0530

    Add files via upload

---
+++ ATM Interface/src/User.java
@@ -0,0 +1,30 @@
+// User class to represent each user of the ATM
+
+public class User {
+
+	private String userID;
+	private int userPIN;
+	private double accountBalance;
+
+	public User(String userID, int userPIN, double accountBalance) {
+		this.userID = userID;
+		this.userPIN = userPIN;
+		this.accountBalance = accountBalance;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public int getUserPIN() {
+		return userPIN;
+	}
+
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(double balance) {
+		this.accountBalance = balance;
+	}
+}
